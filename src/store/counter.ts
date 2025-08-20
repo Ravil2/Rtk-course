88b35b3591c6d8155021ce7ae5e1656411e1d1d4
Ravil2 <rafffa141105@gmail.com>
2025-08-20 11:20:08 +0500

    commit

---
+++ src/store/counter.ts
@@ -0,0 +1,37 @@
+import { createSlice, type Dispatch } from '@reduxjs/toolkit'
+import type { RootState } from './store'
+
+export interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state, action: { payload: number }) => {
+      state.value += action.payload
+    },
+    decrement: (state, action: { payload: number }) => {
+      state.value -= action.payload
+    },
+  },
+})
+
+export const getCount = (state: RootState) => state.counter.value
+
+export const incrementBy = (value: number) => (dispatch: Dispatch) => {
+  dispatch(increment(value))
+}
+
+export const decrementBy = (value: number) => (dispatch: Dispatch) => {
+  dispatch(decrement(value))
+}
+
+export const { increment, decrement } = counterSlice.actions
+
+export default counterSlice.reducer
